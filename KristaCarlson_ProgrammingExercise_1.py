00b8712da401a0f6b196d498d8aed3307cc61868
kcarls10 <Krstcrlsn@gmail.com>
2026-02-08 14:30:53 -0500

    Programming Exercise 1

---
+++ KristaCarlson_ProgrammingExercise_1.py
@@ -0,0 +1,29 @@
+## Takes user input for amount of tickets requested.
+def customer_input():
+    tickets_requested = int(input('Number of Tickets: '))
+    return tickets_requested
+## Keeps track of remaining tickets by subtracting user input amount.
+def remaining(tickets_requested, remaining_tickets):
+    remaining_tickets -= tickets_requested
+    print(f'{remaining_tickets} tickets remaining')
+    return remaining_tickets
+## Main function. Contains while loop, calls functions, validates input, tracks tickets available and buyers.
+def main ():
+    tickets_available = 20
+    buyers = 0
+
+    while tickets_available > 0:
+        tickets_requested = customer_input()
+        if 1 <= tickets_requested <= 4 and tickets_requested <= tickets_available:
+            tickets_available = remaining(tickets_requested, tickets_available)
+            buyers += 1
+        else:
+            print('Invalid input. Maximum tickets allowed (4)')
+
+    print('\nSold out')
+    print(f'Number of buyers: {buyers}')
+
+if __name__ == "__main__":
+    main()
+
+
