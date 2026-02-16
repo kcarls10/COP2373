8f173d8043ed018fcb7de8f1a86dc92dbc89fa81
kcarls10 <krstcrlsn@gmail.com>
2026-02-15 21:20:26 -0500

    Rough draft(3)

--- KristaCarlson_ProgrammingExercise_2.py
+++ KristaCarlson_ProgrammingExercise_2.py
@@ -5,11 +5,11 @@ def spam_email_scanner(email):
     common_keywords = [ "winner", "click here", "prize", "unsubscribe", "congratulations",
                         "pre-qualify", "account suspended", "social security", "compromised",
                         "urgent", "click below", "apply now", "limited time", "free", "cash bonus",
-                        "immediate action", "last call", "prize", "cancel now", "register now", "order now"
+                        "immediate action", "last call", "prize", "cancel now", "register now", "order now",
                         "earn money", "pre-approved", "investment", "exclusive deal", "act now", "debt",
                         "work from home", "don't delete", "cheap", "discount", "million dollars"]
 
-    text = email.text.lower()
+    text = email.lower()
     score = 0
     keywords_found = []
 
@@ -27,3 +27,19 @@ def spam_email_scanner(email):
     else:
         likelihood = "Higher than average likelihood to be spam"
     return likelihood, keywords_found, score
+
+print("Enter suspicious email: ")
+email = ""
+while True:
+     try:
+        line = input()
+        email += line
+     except EOFError:
+        break
+
+likelihood, keywords_found, score = spam_email_scanner(email)
+
+print("Spam Email Scanner Results")
+print(f"Overall Spam Score: {score}")
+print(f"likely Spam Score: {likelihood}")
+print(f"Spam Keywords Found: {', '.join(keywords_found) if keywords_found else 'No keywords found'})")
