e63cd538a29bf465f8a2362408aa0f5ece35ef47
kcarls10 <krstcrlsn@gmail.com>
2026-03-08 18:27:01 -0400

    Draft(1)

--- KristaCarlson_ProgrammingExercise_6.py
+++ KristaCarlson_ProgrammingExercise_6.py
@@ -3,3 +3,20 @@ import re
 def main():
 
     phone = input("Enter a phone number: ")
+    ssn = input("Enter a SSN: ")
+    zip_code = input("Enter a zip code: ")
+
+    if validate_phone(phone):
+        print("Phone number is valid")
+    else:
+        print("Phone number is not valid")
+
+    if validate_ssn(ssn):
+        print("SSN is valid")
+    else:
+        print("SSN is not valid")
+
+    if validate_zip(zip_code):
+        print("Zip code is valid")
+        else:
+        print("Zip code is not valid")
